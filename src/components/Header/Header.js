8e8c321144d49bb2cca026c4d05111c1f2d0959d
Saad Chraibi <saadchraibi85@gmail.com>
2023-12-07 14:53:06 +0100

    added supermenu

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Section, Box, Container, Heading, Text, Flex } from "@radix-ui/themes";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+
+function Header() {
+  return <div></div>;
+}
+
+export default Header;
