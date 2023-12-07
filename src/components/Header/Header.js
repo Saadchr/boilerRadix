3ab220ca09b1fb81840095f2780827aee0547399
Saad Chraibi <saadchraibi85@gmail.com>
2023-12-07 10:52:31 +0100

    added stuff

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,23 +4,22 @@ import Link from "next/link";
 import { ActivityLogIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 function Header() {
   return (
-    <Container grow={"0"} height={"1"} size={"4"}>
-      <Box height={"1"} p={"4"} as="nav">
-        <Flex align={"center"} justify={"between"} gap={"3"}>
-          <ActivityLogIcon />
-          <Flex gap={"4"}>
-            <Link
-              className="no-underline  hover:bg-gray-200 active:bg-gray-300"
-              href="/"
-            >
-              home
-            </Link>
-            <Link href="/about">about</Link>
-            <Link href="/contact">contact</Link>
-          </Flex>
-          <MagnifyingGlassIcon />
+    <Container
+      color="gray"
+      grow={"0"}
+      className="border-4 border-blue-500"
+      size={"4"}
+      p={"4"}
+    >
+      <Flex align={"center"} justify={"between"} gap={"3"}>
+        <ActivityLogIcon />
+        <Flex gap={"4"}>
+          <Link href="/">home</Link>
+          <Link href="/about">about</Link>
+          <Link href="/contact">contact</Link>
         </Flex>
-      </Box>
+        <MagnifyingGlassIcon />
+      </Flex>
     </Container>
   );
 }
